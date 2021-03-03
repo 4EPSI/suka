5c5c44b0982cd2d864219c8b0766310e0685772a
4EPSI <mnatsakanyan@yual.io>
2021-03-03 22:53:04 +0200

    suka mobilka

--- app/js/main.js
+++ app/js/main.js
@@ -1,15 +1,47 @@
-
 // Mobile menu
 $('.burger').on('click', function () {
     $('.burger').toggleClass('active');
     $('body').toggleClass('no_scroll');
-    $('.menu_mob').toggleClass('active');
+    $('.header_menu').toggleClass('active');
 });
 $(document).on('click', function (e) {
     if (!$(e.target).closest('.header').length) {
         $('.burger').removeClass('active');
-        $('.menu_mob').removeClass('active');
+        $('.header_menu').removeClass('active');
         $('body').removeClass('no_scroll');
     }
     e.stopPropagation();
+});
+
+// Scroll to section
+$('body').on('click', '.go_to', function () {
+    var scroll_el = $(this).attr('href');
+    $('html, body').animate({
+        scrollTop: $(scroll_el).offset().top - 80
+    }, 1000);
+    $('.burger').removeClass('active');
+    $('.header_menu').removeClass('active');
+    return false;
+});
+
+// Header
+var viewportheight = $( window ).height(),
+    currentTop = $(this).scrollTop(),
+    header = $('.header').outerHeight();
+
+// Header changing on Load
+if (currentTop >= header) {
+    $('.header').addClass('active');
+} else {
+    $('.header').removeClass('active');
+}
+// Header changing on Scroll
+$(document).scroll(function(){
+    viewportheight = $( window ).height();
+    currentTop = $(this).scrollTop();
+    if (currentTop >= header) {
+        $('.header').addClass('active');
+    } else {
+        $('.header').removeClass('active');
+    }
 });
