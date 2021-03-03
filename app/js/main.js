e695671d236858cc011a56e0393a374b2f4cc3a8
4EPSI <mnatsakanyan@yual.io>
2021-03-04 00:30:47 +0200

    fix suka menu mobilka

--- app/js/main.js
+++ app/js/main.js
@@ -14,34 +14,34 @@ $(document).on('click', function (e) {
 });
 
 // Scroll to section
-$('body').on('click', '.go_to', function () {
-    var scroll_el = $(this).attr('href');
-    $('html, body').animate({
-        scrollTop: $(scroll_el).offset().top - 80
-    }, 1000);
-    $('.burger').removeClass('active');
-    $('.header_menu').removeClass('active');
-    return false;
-});
+// $('body').on('click', '.go_to', function () {
+//     var scroll_el = $(this).attr('href');
+//     $('html, body').animate({
+//         scrollTop: $(scroll_el).offset().top - 80
+//     }, 1000);
+//     $('.burger').removeClass('active');
+//     $('.header_menu').removeClass('active');
+//     return false;
+// });
 
-// Header
-var viewportheight = $( window ).height(),
-    currentTop = $(this).scrollTop(),
-    header = $('.header').outerHeight();
+// // Header
+// var viewportheight = $( window ).height(),
+//     currentTop = $(this).scrollTop(),
+//     header = $('.header').outerHeight();
 
-// Header changing on Load
-if (currentTop >= header) {
-    $('.header').addClass('active');
-} else {
-    $('.header').removeClass('active');
-}
-// Header changing on Scroll
-$(document).scroll(function(){
-    viewportheight = $( window ).height();
-    currentTop = $(this).scrollTop();
-    if (currentTop >= header) {
-        $('.header').addClass('active');
-    } else {
-        $('.header').removeClass('active');
-    }
-});
+// // Header changing on Load
+// if (currentTop >= header) {
+//     $('.header').addClass('active');
+// } else {
+//     $('.header').removeClass('active');
+// }
+// // Header changing on Scroll
+// $(document).scroll(function(){
+//     viewportheight = $( window ).height();
+//     currentTop = $(this).scrollTop();
+//     if (currentTop >= header) {
+//         $('.header').addClass('active');
+//     } else {
+//         $('.header').removeClass('active');
+//     }
+// });
